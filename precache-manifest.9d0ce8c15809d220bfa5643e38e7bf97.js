8b24013febe4e1120c6472c9c9a134b28b718749
BredeJoh <brede_96@hotmail.com>
2019-10-13 10:45:54 +0200

    Updates

---
+++ precache-manifest.9d0ce8c15809d220bfa5643e38e7bf97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ff5f45a95902e4ff4c0bf3845280d0",
+    "url": "/startpage/index.html"
+  },
+  {
+    "revision": "d1c57d941db3b4e3d373",
+    "url": "/startpage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f363f6d0b7c10d3f09f8",
+    "url": "/startpage/static/js/2.a92371db.chunk.js"
+  },
+  {
+    "revision": "d1c57d941db3b4e3d373",
+    "url": "/startpage/static/js/main.9a02086e.chunk.js"
+  },
+  {
+    "revision": "a4823e43b2d636c037c1",
+    "url": "/startpage/static/js/runtime-main.8ac9a900.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/startpage/static/media/logo.25bf045c.svg"
+  }
+]);
