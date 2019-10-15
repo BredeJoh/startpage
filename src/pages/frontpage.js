d95b46ebc002eb5bf36ee33691507a5ec64acbc0
BredeJoh <brede_96@hotmail.com>
2019-10-15 09:16:29 +0200

    Added and tested carousel with bootstrap

--- src/pages/frontpage.js
+++ src/pages/frontpage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './frontpage.css';
+import './frontpage.scss';
 
 class Frontpage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+        
     };
   
   }
@@ -14,32 +15,48 @@ class Frontpage extends React.Component {
       <div className="frontpage">
         <div className="container-fluid">
             <div className="row">
-                <div class="col-sm">
+                <div className="col-sm">
                     One of three columns \\n
                     testOne of three columns \\n
                     testOne of three columns \\n
                     testOne of three columns \\n
                     testOne of three columns \\n
-                    testOne of three columns \\n
-                    testOne of three columns \\n
-                    testOne of three columns \\n
-                    testOne of three columns \\n
-                    te
                 </div>
-                <div class="col-sm">
+                <div className="col-sm">
                     One of three columns
                 </div>
-                <div class="col-sm">
+                <div className="col-sm">
                     One of three columns
                 </div>
             </div>
             <div className="row">
-                <div class="col">
-                    One of two columns
+                <div id="testCarousel" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#testCarousel" data-slide-to="0" className="active"></li>
+                        <li data-target="#testCarousel" data-slide-to="1"></li>
+                        <li data-target="#testCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src="logo512.png" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="testimage_1.png" className="d-block w-100" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="logo192.png" className="d-block w-100" alt="..." />                       
+                        </div>
+                    </div>
+                    <a className="carousel-control-prev" href="#testCarousel" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#testCarousel" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
-                <div class="col-11">
-                    One of two columns
-                </div>
+                
             </div>
         </div>
       </div>
